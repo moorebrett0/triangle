1645b369899a31a34470cb120b1eb79baadd42d0
Brett <student@epicodus.com>
2015-04-10 09:24:42 -0700

    add functionality for actual isoceles test

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,10 @@ var triangle = function(input1, input2, input3) {
     var b = parseInt(input2);
     var c = parseInt(input3);
 
-    if ((a = b) && (a = c) && (b = c)) {
+    if ((a == b) && (a == c) && (b == c)) {
         return "equilateral";
+    }
+    else if ((a == b) || (a == c) || (b==c)) {
+        return "isoceles";
     }
 };
