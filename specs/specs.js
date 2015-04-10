c31f4a0c758f5e91ecbc024a0b7d3d1478216ce2
Brett <student@epicodus.com>
2015-04-10 09:17:44 -0700

    add test for actual isoceles triangle

--- specs/specs.js
+++ specs/specs.js
@@ -2,5 +2,7 @@ describe('triange', function() {
     it("will display equilateral for inputs given 2, 2, 2", function() {
         expect(triangle(2,2,2)).to.equal('equilateral');
     });
-    it("will disp")
+    it("will display an isoceles triangle if two sides are equal", function() {
+        expect(triangle(2,2,3)).to.equal('isoceles');
+    });
 });
