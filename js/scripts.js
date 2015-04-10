30346a06b2f23817e9b40023ac5cac7bb735e5c8
Brett <student@epicodus.com>
2015-04-10 09:12:54 -0700

    add functionality to isoceles test

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var triangle = function(input1, input2, input3) {
+    var a = parseInt(input1);
+    var b = parseInt(input2);
+    var c = parseInt(input3);
+
+    if ((a = b) && (a = c) && (b = c)) {
+        return "isoceles";
+    }
+};
--- specs/specs.js
+++ specs/specs.js
@@ -1,5 +1,5 @@
 describe('triange', function() {
     it("will display isoceles for inputs given 2, 2, 2", function() {
-        expect(triangle('2,2,2')).to.equal('isoceles');
+        expect(triangle(2,2,2)).to.equal('isoceles');
     });
 });
